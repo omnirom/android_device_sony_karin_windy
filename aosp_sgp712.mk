ea84b2c669b51ceb5d13c04556fcec53b29c856e
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-07 19:14:22 +0100

    sgp711: use aosp_base
    follow aosp and use aosp_base on devices that have WiFi only
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_sgp712.mk
+++ aosp_sgp712.mk
@@ -14,6 +14,7 @@
 
 TARGET_KERNEL_CONFIG := aosp_kitakami_karin_windy_defconfig
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, device/sony/karin_windy/aosp_sgp7xx_common.mk)
 
 # Device Init
