85ae4f46fe40d7946d67a9a259215c37c39265bc
Humberto Borba <humberos@gmail.com>
2016-04-06 18:33:33 -0300

    karin: twrp: Update touch dependencies
    All touch dependencies were moved to vendor repo.
    So update copying files process from vendor.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I937bc0a390f2113170fc11df9e2892e27716d728

--- twrp.mk
+++ twrp.mk
@@ -13,27 +13,27 @@ PRODUCT_COPY_FILES += \
 
 # TS config
 PRODUCT_COPY_FILES += \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0000.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0000.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0700.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0700.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0701.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0701.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0702.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0702.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0704.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0704.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0705.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0705.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0706.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0706.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0708.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0708.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0709.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0709.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x070A.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x070A.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x070C.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x070C.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x070D.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x070D.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0710.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0710.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0717.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0717.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x071B.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x071B.cfg \
-    device/sony/karin_windy/rootdir/system/vendor/etc/touch_fusion_panel_id_0x071F.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x071F.cfg
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0000.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0000.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0700.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0700.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0701.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0701.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0702.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0702.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0704.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0704.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0705.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0705.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0706.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0706.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0708.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0708.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0709.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0709.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x070A.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x070A.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x070C.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x070C.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x070D.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x070D.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0710.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0710.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x0717.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0717.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x071B.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x071B.cfg \
+    vendor/sony/kitakami-karin/proprietary/vendor/etc/touch_fusion_panel_id_0x071F.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x071F.cfg
 
 # TS FW loader
 PRODUCT_COPY_FILES += \
-    vendor/sony/karin/proprietary/vendor/bin/touch_fusion:recovery/root/system/vendor/bin/touch_fusion
+    vendor/sony/kitakami-karin/proprietary/vendor/bin/touch_fusion:recovery/root/system/vendor/bin/touch_fusion
 
 # TS FW
 PRODUCT_COPY_FILES += \
-    vendor/sony/karin/proprietary/vendor/firmware/max11945.bin:recovery/root/vendor/firmware/max11945.bin
+    vendor/sony/kitakami-karin/proprietary/vendor/firmware/max11945.bin:recovery/root/vendor/firmware/max11945.bin
